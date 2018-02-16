698a904a53a87deaf8bdb6012cf5646ee1258e36
Gerson Sunyé <gerson.sunye@gmail.com>
2018-02-16 10:46:39 +0100

    more examples

--- Exercices/src/main/java/fr/unantes/agenda/Interval.java
+++ Exercices/src/main/java/fr/unantes/agenda/Interval.java
@@ -5,5 +5,18 @@ package fr.unantes.agenda;
  *
  * @author sunye.
  */
-public class Interval {
+public class Interval<T extends Comparable> {
+
+    private final T begin;
+    private final T end;
+
+    protected Interval(T begin, T end) {
+        this.begin = begin;
+        this.end = end;
+    }
+
+    public boolean includes(T value) {
+        return value.compareTo(begin) >= 0 && value.compareTo(end) <= 0;
+    }
 }
+
